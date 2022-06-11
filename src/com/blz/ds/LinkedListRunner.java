fed259b0c781fa72e43d4793571583a88574920b
Vikash <vikashpathak041@gmail.com>
2022-06-11 18:06:52 +0530

    create linked list and insert some value

--- src/com/blz/ds/LinkedListRunner.java
+++ src/com/blz/ds/LinkedListRunner.java
@@ -5,14 +5,14 @@ public class LinkedListRunner {
     public static void main(String[] args) {
 
         // 10 ==>20==>30==>40==>50
-        LinkedList linkedList = new LinkedList();
+        LinkedList linkedList = new LinkedList();      
 
         linkedList.addLast(56);
         linkedList.addLast(30);
         linkedList.addLast(70);
        
         linkedList.display();
-       
+        
     
        
 
